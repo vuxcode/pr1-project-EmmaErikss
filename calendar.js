c448671053a03daf41d104f0add61f578a40543f
EmmaErikss <emmaaerikssonn@outlook.com>
2025-12-02 17:33:18 +0100

    final style touches on the calendar

--- calendar.js
+++ calendar.js
@@ -1,13 +1,18 @@
+// function for what will happen when the user click on each hatch
 function gift(box) {
 
+    // refer to the date div, and the gift div, box.query to refer to just the one they click.
     var date = box.querySelector(".date");
     var gift = box.querySelector(".gift");
 
+    // if the number is hidden
     if (date.style.display === "none") {
+        // hide gift text and show number on click
         gift.style.display = "none";
         date.style.display = "block";
     }
     else {
+        // or gift text is hidden, hide number and show gift on click.
         date.style.display = "none";
         gift.style.display = "block";
     }
