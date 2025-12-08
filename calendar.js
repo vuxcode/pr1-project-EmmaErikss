410e64883735c895e6bd55eb76bd4d66d0d20090
EmmaErikss <emmaaerikssonn@outlook.com>
2025-12-08 16:16:34 +0100

    locked hatches depending on dates

--- calendar.js
+++ calendar.js
@@ -18,3 +18,29 @@ function gift(box) {
     }
 
 }
+
+// todays date in numbers 
+var now = new Date();
+// Convert todays date into a actual date 
+var today = now.getDate();
+// Refer to all the box divs 
+var boxes = document.querySelectorAll(".box");
+var boxList = [...boxes];
+
+// check each box 
+boxList.forEach(function (box){
+    // use the p value in the boxes 
+    var date = box.innerText;
+    console.log(date);
+
+    // if todays date is less than the p element value 
+    if (today < date) {
+        // the hatch is locked
+        console.log("Locked!");
+        box.classList.add("locked");
+    }
+    // else the hatch is unlocked
+    else{
+        console.log("Unlocked!");
+    }
+});
